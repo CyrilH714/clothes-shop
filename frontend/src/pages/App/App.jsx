41d45ad88d4a0b279b0a9307d8715dd55c8ecfae
CyrilH714 <cyril.j.haller@gmail.com>
2025-06-10 19:11:19 +0100

    set template pages

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router';
 import { getUser } from '../../services/authService';
 import HomePage from '../HomePage/HomePage';
-import PostListPage from '../PostListPage/PostListPage';
-import NewPostPage from '../NewPostPage/NewPostPage';
 import SignUpPage from '../SignUpPage/SignUpPage';
 import LogInPage from '../LogInPage/LogInPage';
 import NavBar from '../../components/NavBar/NavBar';
@@ -16,21 +14,7 @@ export default function App() {
     <main className="App">
       <NavBar user={user} setUser={setUser} />
       <section id="main-section">
-        {user ? (
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/posts" element={<PostListPage />} />
-            <Route path="/posts/new" element={<NewPostPage />} />
-            <Route path="*" element={null} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUpPage setUser={setUser} />} />
-            <Route path="/login" element={<LogInPage setUser={setUser} />} />
-            <Route path="*" element={null} />
-          </Routes>
-        )}
+        
       </section>
     </main>
   );
