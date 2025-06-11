628fdbd916fa33dc85dde6496764b6e2e86087c1
CyrilH714 <cyril.j.haller@gmail.com>
2025-06-11 14:20:23 +0100

    homepage with navigatio buttons to item list categories for filtering

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function HomePage() {
-  return <h1>Home Page</h1>
+  const navigate = useNavigate();
+
+  function goToCategory(category) {
+    navigate(`/items${category ? `?category=${category}` : ''}`);
+  }
+
+  return (
+    <div>
+      <h2>Shop Categories</h2>
+      <button onClick={() => goToCategory('')}>All Products</button>
+            <button onClick={() => goToCategory('top')}>Tops</button>
+      <button onClick={() => goToCategory('outerwear')}>Outerwear</button>
+      <button onClick={() => goToCategory('skirt')}>Skirts</button>
+      <button onClick={() => goToCategory('dress')}>Dresses</button>
+      <button onClick={() => goToCategory('accessory')}>Accessories</button>
+    </div>
+  );
 }
