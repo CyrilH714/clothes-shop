ffa9f395c3da82089f02807dd4785e6a91bc3d47
CyrilH714 <cyril.j.haller@gmail.com>
2025-06-11 12:59:29 +0100

    correctd item model and review model with embedding

---
+++ backend/models/item.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const reviewSchema = new Schema(
+  {
+    author: { 
+        type: mongoose.schema.types.ObjectId,
+         required: true },
+    message: {
+      type: String,
+      required: false,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const itemSchema = new Schema(
+  {
+    name: { 
+        type: String,
+         required: true },
+    type: {
+enum:["dress","top","skirt","outerwear","accessory"],      
+required: true,
+    },
+    brand: {
+      type: String,
+      required: false,
+    },
+    about:{
+        type: String,
+        required: false,
+    },
+    price:{
+        type:Number,
+        required: true,
+  },
+  stock:{
+    type:Number,
+    required:true,
+  },
+  size:{
+    type:String,
+    required:false,
+},
+condition:{
+    enum:["Not worn","Lightly used", "Very used"],
+    required:false,
+},
+imageURL:{
+    type:String,
+required:true,}
+  ,
+ reviews:{
+    type:[reviewSchema],},
+ },{
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  // 'this' is the user document
+  if (!this.isModified('password')) return next();
+  // Replace the password with the computed hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  next();
+});
+
+module.exports = mongoose.model('Item', itemSchema);
